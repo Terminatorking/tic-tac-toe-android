c7ea3dec38c4941e6cb5d4e107a13ed5052a92fa
Soheil <soheilghazimoradi@gmail.com>
2025-06-26 12:02:22 +0330

    Update AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "ghazimoradi.soheil.tictactoe"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ghazimoradi.soheil.tictactoe"
